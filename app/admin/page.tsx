5f2a68f12420a85b016863ffe511f51501f8bb27
matiusherenda <matius292@gmail.com>
2025-09-18 11:35:13 +0700

    update admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold">Admin Page</h1>
+    </main>
+  );
+}
